b80a3d9aef9d8ae98b33a8d966513fcaa4ed1a84
Juliana <juliana.alscher@tufts.edu>
2020-12-14 11:35:26 -0500

    attempt to do buttons

--- src/secret.js
+++ src/secret.js
@@ -16,8 +16,8 @@ var getAPIusername = ""; // fill in with your name (if left blank, assuming usin
 // (2) Hardcode your API key information here
 
 // AIRTABLE SUPPORT
-var airtable_api = "key" + "XXXXXXXXXXX";     // fill in your info
-var airtable_baseid = "app" + "YYYYYYYYYYYY"; // fill in your info
+var airtable_api = "keyQA5kun7Gwx00y0";     // fill in your info
+var airtable_baseid = "appzdJvc1AihJOTyI"; // fill in your info
 
 // SYSTEMLINK SUPPORT
 var systemlink_api = "XXXXXXXXXXXXXXXXXX"; // fill in your info
